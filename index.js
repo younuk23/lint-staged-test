5fd04fbdb6dea4412871c889b448c608d2c20ace
Yeonuk Hwang <younuk23@gmail.com>
2022-01-06 11:15:15 +0900

    Add: git hook

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+// eslint에서 console.log는 warning만 출력하기 때문에 eslint에서 pass 해줍니다.
+// warning도 허용하고 싶지 않다면 package.json에서 lint-staged안의 eslint 명령어를 eslint --max-warnings=0 으로 변경하면
+// warning이 하나라도 출력되면 eslint에서 패스하지 못합니다.
+// package.strict-sample.json 파일 참고
 console.log('eslint warning');
 
-const unusedVar = 'eslint error';
+// 주석 풀면 eslint no-unused-var 룰에 걸려서 commit을 못합니다.
+// const unusedVar = 'eslint error'
 
 const sum = (x, y) => x + y;
 
--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ const { sum } = require('./index');
 test('test sum function, 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+// // 주석 풀면 pre-push hook에서 test 통과 못해서 push가 막힙니다.
+// test("this will fail",() => {
+//   expect(sum(1,2)).toBe(4);
+// })
